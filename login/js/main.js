9c5582a39353278c0acaf5d1ac111e499ff0ed4f
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-04-14 15:54:39 -0400

    debug4

--- login/js/main.js
+++ login/js/main.js
@@ -1,6 +1,7 @@
 
 	function onSignIn(googleUser) {
 		var profile = googleUser.getBasicProfile();
+		console.log(profile);
 		console.log('ID: ' + profile.getId());
 		console.log('Name: ' + profile.getName());
 		console.log('Image URL: ' + profile.getImageUrl());
